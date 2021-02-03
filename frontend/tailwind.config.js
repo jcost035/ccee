3729ed22fbb32f76375a7aa107ed055bba06617c
Jorge Costa <jcost035@fiu.edu>
2021-02-02 21:29:18 -0500

    navbar moved out + panels

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,6 +13,18 @@ module.exports = {
             },
             spacing: {
                 '170': '1700px',
+            },
+            colors: {
+                cblue: {
+                    light: '#74BEE9',
+                    DEFAULT: '#0071BC',
+                    dark: '#004A80',
+                },
+                cgreen: {
+                    DEFAULT: '#88c23d',
+                    dark: '#588527',
+                },
+                cgray: '#414042',
             }
         },
     },
