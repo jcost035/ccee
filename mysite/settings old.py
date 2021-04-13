4a0ec468bd2878bab3e63ec59d024aa8ee2d3184
Jorge Costa <jcost035@fiu.edu>
2021-04-13 02:14:38 -0400

    stuff

--- mysite/settings old.py
+++ mysite/settings old.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +127,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 MEDIA_URL = '/mysite/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "mysite/media")
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
