b6135fa368ff68e4a64c23b2b43bec4837c5f0c0
Jorge Costa <jcost035@fiu.edu>
2021-06-18 22:25:54 -0400

    idk

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['warm-bayou-59199.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ccee.org', 'warm-bayou-59199.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
